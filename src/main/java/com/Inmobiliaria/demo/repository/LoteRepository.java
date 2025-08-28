ae86d70cc2f43ae32ba006d23df01714098fe4ef
Bytauro3000 <bytauro3000@gmail.com>
2025-08-28 12:42:19 -0500

    Team

--- src/main/java/com/Inmobiliaria/demo/repository/LoteRepository.java
+++ src/main/java/com/Inmobiliaria/demo/repository/LoteRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Inmobiliaria.demo.entity.Lote;
-
+//Repo
 @Repository
 public interface LoteRepository extends JpaRepository<Lote, Integer>{
 	List<Lote> findByProgramaIdPrograma(Integer idPrograma);
